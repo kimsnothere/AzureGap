14ae621853275a2afde412dea062cb3a2b81e369
kim szekely <kimszekely319@outlook.com>
2019-09-21 23:23:20 -0400

    created project

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+  <span class='message'>AzureGap</span>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+.home > img {
+  height: 20px;;
+}
+
+.message {
+  font-size: 80px;
+}
+</style>
